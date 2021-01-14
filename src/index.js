7da45dd78ad066bef6d915295eff1c489e833d39
karrli <krla.ici@gmail.com>
2021-01-14 15:46:39 -0600

    First trivia game designed using OOP

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import './styles/main.scss';
+import Trivia from './trivia';
+import _ from 'lodash';
+
+
+function getTriviaQuestions() {
+  axios.get('https://opentdb.com/api.php?amount=10')
+  .then(function (response) {
+    // handle success
+    // console.log(response.status) 
+    console.log(response.data, "esta es la data") 
+    // console.log(response.data.results.length)
+    console.log(response.data.results)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+}
+
+
+getTriviaQuestions();
+
+
+let callApiButton = document.getElementById("submit");
+
+
+callApiButton.addEventListener("click", function() { 
+  let category = document.getElementById("trivia_category").value;
+  let difficulty = document.getElementById("trivia_difficulty").value;
+  let type = document.getElementById("trivia_type").value;
+console.log(category)
+console.log(difficulty)
+console.log(type)
+  axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`)
+.then(function (response) {
+  // handle success
+  console.log(response.status)
+  console.log(response.data) 
+
+  const trivia = new Trivia(response.data.results)
+  trivia.buildTrivia();
+  // trivia.renderQuestions();
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+}
+
+)
---
+++ src/trivia.js
@@ -0,0 +1,76 @@
+const TRIVIA_CONTAINER = document.getElementById("questions-container");
+
+class Trivia {
+    constructor(questions) {
+        this.questions = questions;
+        console.log(this.questions, "testing")
+        //    this.questions.incorrect_answers = incorrectAnswers
+        //    this.questions.correct_answers = correctAnswers
+    }
+
+    getAnswers() {
+        for (let i = 0; i < this.questions.length; i++) {
+            let answers = [...this.questions[i].incorrect_answers, this.questions[i].correct_answer]
+            // answers.push(this.questions[i].correct_answer)
+
+            return answers, console.log(answers, "answers array test")
+
+        }
+    }
+
+    buildTrivia() {
+
+        console.log(this.questions, "hola");
+        for (let i = 0; i < this.questions.length; i++) {
+            const questionContainer = document.createElement("div");
+            const answersContainer = document.createElement("ul");
+            const buttonsContainer = document.createElement("div");
+            const submitButton = document.createElement("button");
+            const nextButton = document.createElement("button");
+            // const answerList = document.createElement("li");
+
+            questionContainer.classList.add("d-flex", "justify-content-center");
+            questionContainer.innerHTML = this.questions[i].question
+            console.log(this.questions[i].question)
+            answersContainer.classList.add("list-group", "mt-3");
+            buttonsContainer.classList.add("row", "d-flex", "justify-content-center");
+            submitButton.classList.add("btn", "btn-primary", "mr-3");
+            submitButton.innerHTML = "Submit";
+            nextButton.classList.add("btn", "btn-success");
+            nextButton.innerHTML = "Next";
+            // answerList.classList.add("list-group-item", "text-center")
+
+            // answerList.innerHTML = this.getAnswers()
+
+            let answers = [...this.questions[i].incorrect_answers, this.questions[i].correct_answer]
+            console.log(this.questions[i].incorrect_answers, "wrong", this.questions[i].correct_answer, "right")
+            let shuffledAnswers = _.shuffle(answers)
+            // answers.push(this.questions[i].correct_answer)
+            // return answers.map((answer) =>{answerList.innerHTML = answer, console.log(answerList.innerHTML = answer, "answers array")}) 
+            for (let a in shuffledAnswers) {
+                let answerList = document.createElement('li');
+                answerList.id = shuffledAnswers[a];
+                answerList.classList.add("list-group-item", "text-center");
+                answerList.innerHTML = shuffledAnswers[a];
+                // document.body.appendChild(answerList);
+
+                answersContainer.append(answerList);
+                TRIVIA_CONTAINER.append(questionContainer);
+                TRIVIA_CONTAINER.append(answersContainer);
+            }
+            buttonsContainer.append(submitButton);
+            buttonsContainer.append(nextButton);
+            TRIVIA_CONTAINER.append(buttonsContainer);
+            answersContainer.append(answerList); //if disabled all questions are shown 
+            // TRIVIA_CONTAINER.append(questionContainer);
+            // TRIVIA_CONTAINER.append(answersContainer);
+        }
+    }
+
+    Next() {
+
+    }
+
+}
+
+export default Trivia;
