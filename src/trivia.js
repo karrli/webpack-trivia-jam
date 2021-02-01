2b3fada58149e81a75a26bee11fd7ad738d425a9
Karla Solares <krla.ici@gmail.com>
2021-01-31 23:37:51 -0600

    Deleted spaces and updated ternary

--- src/trivia.js
+++ src/trivia.js
@@ -1,16 +1,11 @@
 const TRIVIA_CONTAINER = document.getElementById("questions-container");
-
-
 class Trivia {
     constructor(questions) {
         this.questions = questions;
         console.log(this.questions, "testing")
         this.currentQuestionIndex = 0
-        this.selectedValue = null
-        
+        this.selectedValue = null       
     }
-
-
 
     buildTrivia(questionObject) {
 
@@ -21,7 +16,6 @@ class Trivia {
             const buttonsContainer = document.createElement("div");
             const submitButton = document.createElement("button");
             const nextButton = document.createElement("button");
-
 
             questionContainer.classList.add("d-flex", "justify-content-center");
             questionContainer.innerHTML = this.questions[this.currentQuestionIndex].question
@@ -46,66 +40,36 @@ class Trivia {
                 this.buildTrivia(this.questions[this.currentQuestionIndex].question)
                 console.log(this.questions[this.currentQuestionIndex].question, "testing next")
             })
-            
-            
-            
-  
 
             let answers = [...this.questions[this.currentQuestionIndex].incorrect_answers, this.questions[this.currentQuestionIndex].correct_answer]
             console.log(...this.questions[this.currentQuestionIndex].incorrect_answers, "wrong", this.questions[this.currentQuestionIndex].correct_answer, "right")
             let shuffledAnswers = _.shuffle(answers)
-            console.log(shuffledAnswers, "shuffled")
-            
-           
+                      
             let answerList = []
             for (let a in shuffledAnswers) {
                 answerList = document.createElement('li');
                 answerList.id = shuffledAnswers[a];
-                answerList.classList.add("list-group-item", "text-center");
-               
-                answerList.innerHTML = shuffledAnswers[a];
-                
+                answerList.classList.add("list-group-item", "text-center");               
+                answerList.innerHTML = shuffledAnswers[a];              
                 answerList.addEventListener("click", (e) => {
                     const selectedChoice = e.target;
                     this.selectedValue = shuffledAnswers[a]
                     submitButton.disabled = false;
-                    if (this.selectedValue === this.questions[this.currentQuestionIndex].correct_answer ) {
-                    selectedChoice.style.backgroundColor = "#abf0d1" }
-                    else {
-                        selectedChoice.style.backgroundColor = "#ce4d4d" 
-                    }
-                    console.log(e.target.value, "aaahh")
-                    
-                    console.log(this.selectedValue, "value")
-               
-                 
+                    this.selectedValue === this.questions[this.currentQuestionIndex].correct_answer ? 
+                    selectedChoice.style.backgroundColor = "#abf0d1" : selectedChoice.style.backgroundColor = "#ce4d4d"                                                   
                 })
-                
-               
-            
-                
 
                 answersContainer.append(answerList);
                 TRIVIA_CONTAINER.append(questionContainer);
                 TRIVIA_CONTAINER.append(answersContainer);
             }
-            console.log(answersContainer)
         
             buttonsContainer.append(submitButton);
             buttonsContainer.append(nextButton);
             TRIVIA_CONTAINER.append(buttonsContainer);
             answersContainer.append(answerList); //if disabled all questions are shown 
-            submitButton.disabled = true;
-        
-    }
-    
-    
-
-  
-
-
-
-    
+            submitButton.disabled = true;        
+    }   
 }
 
 export default Trivia;
